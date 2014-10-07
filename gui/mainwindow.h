f63cb664d76e48c33e5ca89651895e412e115d55
bhattigurjot <bhattigurjot@gmail.com>
2014-10-07 23:32:23 +0530

    adds toggle function for menu items

--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -48,6 +48,8 @@ private slots:
     void on_actionZoom_Out_triggered();
     void on_actionInsert_Image_triggered();
 
+    void toggleActions(bool b);
+
     void filePrintPreview();
     void filePrint();
     void print(QPrinter *);
