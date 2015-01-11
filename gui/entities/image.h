ab6471bb956249b00af9b1c2f424bbd97a0ee19f
kamalpreetgrewal <grewalkamal005@gmail.com>
2015-01-11 17:22:17 +0530

    corrects license and adds license preamble to all files

--- gui/entities/image.h
+++ gui/entities/image.h
@@ -1,3 +1,22 @@
+/**
+ * image.h
+ *
+ * Copyright (C) 2014 GreatDevelopers
+ *
+ * eCAD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eCAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef IMAGE_H
 #define IMAGE_H
 
